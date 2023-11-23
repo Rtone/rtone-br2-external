1c1f7ab77477c79d053b8333dbdf2c963c7bce8e
Gaël PORTAY <gael.portay@rtone.fr>
2023-11-23 21:11:40 +0100

    board: raspberrypi4: add dual bank partition scheme and autoboot.txt
    This duplicates the boot and rootfs partitions to add a dual bank
    partition scheme and adds the support for autoboot.txt[1].
    
    According to "The config.txt file" documentation:
    
    	autoboot.txt
    
    	autoboot.txt is an optional configuration file that can be used
    	to specify the boot_partition number.
    
    	This can also be used in conjunction with the tryboot feature to
    	implement A/B booting for OS upgrades.
    
    	autoboot.txt is limited to 512 bytes and supports the [all],
    	[none] and [tryboot] conditional filters.
    
    	See also TRYBOOT boot flow.
    
    Edit the file /boot/autoboot/autoboot.txt and set boot_partition=3 to
    switch to bank B for the next reboots (and re-set boot_partition=2 to
    switch back to bank A).
    
    [1]: https://www.raspberrypi.com/documentation/computers/config_txt.html#autoboot-txt

---
+++ board/raspberrypi/post-image.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+BOARD_DIR="$(dirname $0)"
+GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+cp "${BINARIES_DIR}/rpi-firmware/cmdline.txt" "${BINARIES_DIR}/rpi-firmware/autoboot.txt"
+echo "root=/dev/mmcblk0p5 rootwait console=tty1 console=serial0" >"${BINARIES_DIR}/cmdline-a.txt"
+echo "root=/dev/mmcblk0p6 rootwait console=tty1 console=serial0" >"${BINARIES_DIR}/cmdline-b.txt"
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage \
+	--rootpath "${TARGET_DIR}"     \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${GENIMAGE_CFG}"
+
+exit $?
