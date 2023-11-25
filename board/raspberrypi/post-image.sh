70002aa287abaf0d210fe122574fc134923a2b21
Gaël PORTAY <gael.portay@rtone.fr>
2023-11-25 14:05:58 +0100

    board/raspberrypi: add RAUC backend for raspberrypi firmware
    This enables RAUC support by implementing a custom bootloader backend[1]
    for the raspberrypi firmware[2][3][4] and by creating a RAUC bundle to
    update the rootfs slots.
    
    Note: The custom bootloader backend APIs for the raspberrypi firmware
    are self-documented in shell script raspberrypi-firmware-backend.sh.
    
    Important: The CA[5], the certificate[6] and the private key[7] are
    imported from RAUC tree[8]. ***DON'T USE THEM IN PRODUCTION!!!***
    
    [1]: https://rauc.readthedocs.io/en/latest/integration.html#custom
    [2]: https://www.raspberrypi.com/documentation/computers/config_txt.html#autoboot-txt
    [3]: https://www.raspberrypi.com/documentation/computers/raspberry-pi.html#fail-safe-os-updates-tryboot
    [4]: https://www.raspberrypi.com/documentation/computers/config_txt.html#example-update-flow-for-ab-booting
    [5]: https://github.com/rauc/rauc/blob/aa6e2d8930f6eb23ae94ca466d3525984c057e48/test/openssl-ca/rel-ca.pem
    [6]: https://github.com/rauc/rauc/blob/f1c5da71700cd7124d2b5bf5e76d2581cee73f2f/test/openssl-ca/rel/release-1.cert.pem
    [7]: https://github.com/rauc/rauc/blob/f1c5da71700cd7124d2b5bf5e76d2581cee73f2f/test/openssl-ca/rel/private/release-1.pem
    [8]: https://github.com/rauc/rauc/tree/f2710effb756b05cdf41c6e2f65fe409f88eb94b/test

--- board/raspberrypi/post-image.sh
+++ board/raspberrypi/post-image.sh
@@ -6,6 +6,7 @@ BOARD_DIR="$(dirname $0)"
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
+cp "${BOARD_DIR}/rel-ca.pem" "${BOARD_DIR}/release-1.cert.pem" "${BOARD_DIR}/release-1.pem" "${BINARIES_DIR}"
 cp "${BOARD_DIR}/autoboot-a.txt" "${BINARIES_DIR}"
 echo "root=/dev/mmcblk0p5 rootwait console=tty1 console=serial0" >"${BINARIES_DIR}/cmdline-a.txt"
 echo "root=/dev/mmcblk0p6 rootwait console=tty1 console=serial0" >"${BINARIES_DIR}/cmdline-b.txt"
