1990bd02a04f2c893bcd2b0c3c6cf691914ce933
Gaël PORTAY <gael.portay@rtone.fr>
2024-04-12 13:29:18 +0200

    Makefile: add bmaptool-copy-<blkname> target

--- external.mk
+++ external.mk
@@ -20,3 +20,11 @@ rm-target:
 	rm -rf $(BUILD_DIR)/*/.stamp_target_installed
 endif
 endif
+
+ifndef ($(BASE_TARGET_DIR),)
+define bmaptool-copy
+bmaptool-copy-$(1):
+	sudo bmaptool copy $$(BINARIES_DIR)/sdcard.img /dev/$(1)
+endef
+$(foreach devname,$(shell ( cd /sys/block && ls )),$(eval $(call bmaptool-copy,$(devname))))
+endif
