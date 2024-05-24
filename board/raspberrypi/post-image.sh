d5b97a8a961071a3fe2c19587b57648bdb6ef21e
Gaël PORTAY <gael.portay@rtone.fr>
2024-05-24 14:24:27 +0200

    board/raspberrypi: genimage.cfg: remove autoboot-{a,b}.txt
    This fixes commit ed55cac000661bc19fdb8d96ba59680b8e704e3a.
    
    This removes the artifacts autoboot-a.txt and autoboot-b.txt that are no
    longer needed since ed55cac000661bc19fdb8d96ba59680b8e704e3a.

--- board/raspberrypi/post-image.sh
+++ board/raspberrypi/post-image.sh
@@ -6,7 +6,7 @@ BOARD_DIR="$(dirname $0)"
 GENIMAGE_CFG="${BOARD_DIR}/genimage.cfg"
 GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
 
-cp "${BOARD_DIR}/autoboot-a.txt" "${BOARD_DIR}/autoboot-b.txt" "${BINARIES_DIR}"
+cp "${BOARD_DIR}/autoboot-a.txt" "${BINARIES_DIR}"
 echo "root=/dev/mmcblk0p5 rootwait console=tty1 console=serial0" >"${BINARIES_DIR}/cmdline-a.txt"
 echo "root=/dev/mmcblk0p6 rootwait console=tty1 console=serial0" >"${BINARIES_DIR}/cmdline-b.txt"
 
