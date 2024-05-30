eb1eab5b16c0af20cb2c17ea5b31fde38962aa1b
Gaël PORTAY <gael.portay@rtone.fr>
2024-05-30 13:50:17 +0200

    external: add paranoid tests
    This adds paranoid tests to avoid removal things if empty variables.

--- external.mk
+++ external.mk
@@ -11,12 +11,15 @@ nothing:
 ifneq ($(BUILD_DIR),)
 .PHONY: rm-build
 rm-build:
+	test -d "$(BUILD_DIR)"
 	rm -rf $(BUILD_DIR)/*/*
 
 ifneq ($(BASE_TARGET_DIR),)
 .PHONY: rm-target
 rm-target:
+	test -d "$(BASE_TARGET_DIR)"
 	rm -rf $(BASE_TARGET_DIR)
+	test -d "$(BUILD_DIR)"
 	rm -rf $(BUILD_DIR)/*/.stamp_target_installed
 endif
 endif
@@ -24,6 +27,7 @@ endif
 ifneq ($(BASE_TARGET_DIR),)
 define bmaptool-copy
 bmaptool-copy-$(1):
+	test -b "/dev/$(1)"
 	sudo bmaptool copy $$(BINARIES_DIR)/sdcard.img /dev/$(1)
 endef
 $(foreach devname,$(shell ( cd /sys/block && ls )),$(eval $(call bmaptool-copy,$(devname))))
